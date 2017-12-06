1f0e1ac5c5be075993f808456b982677861fd105
runzexia <root@junwuhui.cn>
2017-12-06 18:01:24 +0800

    fix go lint

--- request/signer.go
+++ request/signer.go
@@ -99,7 +99,7 @@ func (is *Signer) BuildStringToSign(request *http.Request) (string, error) {
 	}
 	return "", fmt.Errorf("Requset Type Not Support For Sign ")
 }
-// BuildStringToSign build the string to sign.
+// BuildStringToSignByValues build the string to sign.
 func (is *Signer) BuildStringToSignByValues(requestDate string, requestMethod string, requestPath string, requestParams url.Values) (string, error) {
 	requestParams.Set("access_key_id", is.AccessKeyID)
 	requestParams.Set("signature_method", "HmacSHA256")
