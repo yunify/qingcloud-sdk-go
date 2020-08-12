e11bcc1c0562e41913889c8846011a85aaaa1089
Feiran <feiranyang@yunify.com>
2020-08-12 17:24:38 +0800

    fix comment

--- request/request.go
+++ request/request.go
@@ -39,16 +39,16 @@ type Request struct {
 	HTTPResponse *http.Response
 }
 
-// Default credential proxy host
+// DefaultCredentialProxyHost is default credential proxy host
 const DefaultCredentialProxyHost = "169.254.169.254"
-// Default credential proxy port
+// DefaultCredentialProxyPort is default credential proxy port
 const DefaultCredentialProxyPort = 80
-// Default credential proxy protocol
+// DefaultCredentialProxyProtocol is default credential proxy protocol
 const DefaultCredentialProxyProtocol = "http"
-// Default credential proxy URI
+// DefaultCredentialProxyURI is default credential proxy URI
 const DefaultCredentialProxyURI = "/latest/meta-data/security-credentials"
 
-// The structure of token when retrieving it
+// TokenOutput is the structure of token when retrieving it
 type TokenOutput struct {
 	Jti string `json:"jti"`
 	Token string `json:"id_token"`
@@ -206,7 +206,7 @@ func (r *Request) unpack() error {
 	return nil
 }
 
-// Get token from credential proxy server
+// GetToken is used to get token from credential proxy server
 func (t *TokenOutput) GetToken(credentialProxyURL string) error{
 	response, err := http.Get(credentialProxyURL)
 	if err != nil {
